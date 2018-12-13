aa61ac217ec48fa37570aa99f41dc79b4914ce75
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2018-12-13 17:03:12 +0000

    Add support for external repo

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,11 @@ export PATH=/home/imaxt/.local/bin:$PATH
 
 if [ "$EXTRA_PIP_PACKAGES" ]; then
     echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
-    /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES
+    if [ "$EXTRA_INDEX_URL" ]; then
+        /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES --extra-index-url=$EXTRA_INDEX_URL
+    else
+        /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES
+    fi
 fi
 
 # Run extra commands
