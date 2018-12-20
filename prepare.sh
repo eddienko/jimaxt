013de8be549e4544c52c646ae231c7cce525b191
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2018-12-20 08:50:20 +0000

    Simplify prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -6,11 +6,7 @@ export PATH=/home/imaxt/.local/bin:$PATH
 
 if [ "$EXTRA_PIP_PACKAGES" ]; then
     echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
-    if [ "$EXTRA_INDEX_URL" ]; then
-        /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES --extra-index-url=$EXTRA_INDEX_URL
-    else
-        /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES
-    fi
+    /opt/conda/bin/pip install --user $EXTRA_PIP_PACKAGES
 fi
 
 # Run extra commands
